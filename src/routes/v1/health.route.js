474833823562bca5cfbb00e827862ac2b26d13ac
Erick <linarespachecoerick@gmail.com>
2023-04-22 19:51:11 -0400

    Update health route to show a text message to the user

--- src/routes/v1/health.route.js
+++ src/routes/v1/health.route.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const httpStatus = require('http-status');
 
 const router = express.Router();
 
-router.all('/', (_, res) => res.status(200).send());
+router.all('/', (_, res) =>
+  res.status(httpStatus.OK).send({
+    message: 'En hora buena, nuestra app funciona.',
+    status: 'live',
+  })
+);
 
 module.exports = router;
