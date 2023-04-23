cf2511721a867ea3e8754cdedf64f0d937547c8f
Erick <linarespachecoerick@gmail.com>
2023-04-22 23:29:42 -0400

    Create tagsSchema Model

---
+++ src/models/tag.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const tagSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Tag = mongoose.model('Tag', tagSchema);
+
+module.exports = Tag;
