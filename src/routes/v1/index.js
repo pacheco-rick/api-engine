a7f2f88a13332547dbdaeb0bf4db329bf96a6f6f
Erick <linarespachecoerick@gmail.com>
2023-04-22 23:54:08 -0400

    Tag route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,7 @@ const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const docsRoute = require('./docs.route');
 const healthRoutes = require('./health.route');
+const tagRoutes = require('./tag.route');
 const config = require('../../config/config');
 
 const router = express.Router();
@@ -19,6 +20,10 @@ const defaultRoutes = [
   {
     path: '/health',
     route: healthRoutes,
+  },
+  {
+    path: '/tags',
+    route: tagRoutes,
   },
 ];
 
